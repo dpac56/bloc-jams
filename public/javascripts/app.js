d9b64bef2302174858cdc06d2255216c026f108a
deepak <deepak@withstartups.com>
2014-09-05 20:05:14 +0530

    Added an overlay to the collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -174,17 +174,19 @@ var changeAlbumView = function(album){
 
 ;require.register("scripts/app", function(exports, require, module) {
 require("./landing");
-<<<<<<< HEAD
 require('./album');
-=======
 require("./collection");
+
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
 var buildAlbumThumbnail = function(){
    var template =
         '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png" class="img-responsive"/>'
+      + '  <div class="collection-album-image-container">'
+      + '    <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -200,6 +202,23 @@ var buildAlbumThumbnail = function(){
     return $(template);
 };
 
+var buildAlbumOverlay = function(albumURL) {
+  var template =
+      '<div class="collection-album-image-overlay">'
+    + '  <div class="collection-overlay-content">'
+    + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+    + '      <i class="fa fa-play"></i>'
+    + '    </a>'
+    + '    &nbsp;'
+    + '    <a class="collection-overlay-button">'
+    + '      <i class="fa fa-plus"></i>'
+    + '    </a>'
+    + '  </div>'
+    + '</div>'
+    ;
+  return $(template);
+};
+
 var updateCollectionView = function(){
   var $collection = $(".collection-container .row");
   $collection.empty();
@@ -208,6 +227,16 @@ var updateCollectionView = function(){
    var $newThumbnail = buildAlbumThumbnail();
    $collection.append($newThumbnail);
 }
+
+   var onHover = function(event) {
+   $(this).append(buildAlbumOverlay("/album.html"));
+ };
+
+  var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+
+  $collection.find('.collection-album-image-container').hover(onHover, offHover);
 };
 
 if (document.URL.match(/\/collection.html/)) {
@@ -216,7 +245,6 @@ if (document.URL.match(/\/collection.html/)) {
     updateCollectionView();
   });
 }
->>>>>>> collection-jquery
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
